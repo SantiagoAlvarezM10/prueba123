17e66b06a292240a7aa27ef05e886e51b53b6378
ALVAREZ\USUARIO <sanalvarezmun@unal.edu.co>
2017-04-06 22:58:27 -0500

    no message

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -5,7 +5,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="utf-8">
 	<title>Welcome to CodeIgniter</title>
-	<title>Welcome to ahora si CodeIgniter</title>
 
 	<style type="text/css">
 
